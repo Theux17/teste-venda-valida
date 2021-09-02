b2a5f76928eebe519d38c09d7ed18e964138e333
Theux17 <oliveira.ms18@gmail.com>
2021-09-02 15:46:21 -0300

    receiving form data

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,8 +1,20 @@
 import "dotenv/config"
+import path from 'path'
 import express from 'express'
+import { urlencoded } from "body-parser"
 import { routes }  from './routes'
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, "..", "..", "front-end")))
+app.set("views", path.join(__dirname, "..",  "..", "front-end"))
+app.engine("html", require("ejs").renderFile)
+app.set("view engine", "html")
+app.use(urlencoded({ extended: true }))
+
+app.get("/forms", (request, response) => {
+  return response.render("index.html")
+})
 
 app.use(express.json())
 app.use(routes)
